28c92bf77275c118279982a5a7d800969564748c
Enderson Vizcaino <enjaviga@gmail.com>
2018-12-17 17:21:25 -0500

    Variable de entorno MONGO :muscle:

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://endersonpro:14Junio98*@ds237574.mlab.com:37574/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
